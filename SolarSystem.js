d752745639d65073a200a33d33914be20d5a2625
Tommy Waters <Thomas.Waters05@gmail.com>
2016-08-20 11:27:31 -0500

    push up

---
+++ SolarSystem.js
@@ -0,0 +1,30 @@
+var SolarSystem = (function(){
+  var planets = ["Mercury", "Venus", "Earth", "Jupiter", "Uranus", "Saturn", "Neptune"];
+  var activeSpacecraft = ["Voyager I"];
+  var planetsLandedOn = 0;
+
+return {
+     spaceDate: new Date(),
+     getPlanets: function(){
+     return planets
+     },
+     setPlanets: function(newPlanets){
+      planets.push(newPlanet);
+
+     },
+     getActiveSpacecraft: function(){
+      return activeSpacecraft;
+
+     },
+     setActiveSpacecraft: function(newSpacecraft){
+      activeSpacecraft.push(newSpacecraft);
+
+     },
+     getplanetsLandedOn: function(){
+        return planetsLandedOn;
+     },
+     setplanetsLandedOn: function(numLanded){
+      planetsLandedOn = numLanded;
+     }
+    }
+})(); //not working for some reason..says SolarSytem undefined?
---
+++ main.js
@@ -0,0 +1,16 @@
+//1. public property for holding the last modified date
+//2. getter/setter for planets
+//3. getter/setter for activeSpacecraft
+//4. getter/setter for planetsLandedOn
+
+console.log(SolarSystem);
+console.log(SolarSystem.getPlanets());
+console.log(SolarSystem.getActiveSpacecraft());
+console.log(SolarSystem.planetsLandedOn());
+
+
+
+
+
+
+
