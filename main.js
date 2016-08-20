b64b05ad4a769b9e70cdcec48c4e380eef436dbd
Tommy Waters <Thomas.Waters05@gmail.com>
2016-08-20 12:25:35 -0500

    oops

---
+++ main.js
@@ -0,0 +1,17 @@
+//1. public property for holding the last modified date
+//2. getter/setter for planets
+//3. getter/setter for activeSpacecraft
+//4. getter/setter for planetsLandedOn
+
+console.log(SolarSystem);
+console.log(SolarSystem.getPlanets());
+console.log(SolarSystem.getActiveSpacecraft());
+console.log(SolarSystem.getplanetsLandedOn());
+
+console.log(SolarSystem.getclosestStars());
+SolarSystem.setageOfSolarSystem(100);
+console.log(SolarSystem.setageOfSolarSystem());
+console.log(SolarSystem.getdwarfPlanets());
+
+
+
